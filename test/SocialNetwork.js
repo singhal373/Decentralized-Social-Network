3d2184cc0c39d6abcc683330dacc6ffc6ce1b25b
Tanya3108 <tanyasanjaykumar@gmail.com>
2021-10-29 15:32:16 +0400

    Added tipping post functionality

--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -48,5 +48,45 @@ contract('SocialNetwork', ([deployer, author, tipper]) => {
       await socialNetwork.createPost('', { from: author }).should.be.rejected;
     })
 
+    it('lists posts', async() => {
+      const post = await socialNetwork.posts(postCount)
+      assert.equal(post.id.toNumber(),postCount.toNumber(),"ID verified")
+      assert.equal(post.content, 'This is my first post', 'content verification complete')
+      assert.equal(post.tipAmount, '0', 'tip amount verified')
+      assert.equal(post.author, author, 'author verification complete')
+    })
+
+    it('allow tipping', async()=>{
+
+      let originalBal
+      originalBal = await web3.eth.getBalance(author)
+      originalBal = new web3.utils.BN(originalBal)
+
+      result = await socialNetwork.tipPost(postCount, { from: tipper, value: web3.utils.toWei('1', 'Ether') })
+
+      // SUCESS
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), postCount.toNumber(), "ID verified")
+      assert.equal(event.content, 'This is my first post', 'content verification complete')
+      assert.equal(event.tipAmount, '1000000000000000000', 'tip amount verified')
+      assert.equal(event.author, author, 'author verification complete')
+
+      // Check that author received funds
+      let newBal
+      newBal = await web3.eth.getBalance(author)
+      newBal = new web3.utils.BN(newBal)
+
+      let tipAmount
+      tipAmount = web3.utils.toWei('1', 'Ether')
+      tipAmount = new web3.utils.BN(tipAmount)
+
+      const exepectedBalance = originalBal.add(tipAmount)
+
+      assert.equal(newBal.toString(), exepectedBalance.toString())
+
+      // FAILURE: Tries to tip a post that does not exist
+      await socialNetwork.tipPost(99, { from: tipper, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+
+    })
   })
 })
